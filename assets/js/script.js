b8aac58e5871806ef5ac1250afb6d0578d04b33b
w.hunt <wfh2d88@gmail.com>
2021-01-16 10:39:52 -0600

    Module 5 done

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,7 +118,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -199,16 +199,18 @@ $(".list-group").on("click", "span", function(){
     tolerance: "pointer", 
     helper: "clone" , 
     activate: function(event) {
-      console.log("activate" , this);
+      $(this).addClass("dropover");
+      $(".bottom-trash").addClass("bottom-trash-drag");
     },
     deactivate: function(event) {
-      console.log("deactivate", this);
+      $(this).removeClass("dropover");
+      $(".bottom-trash").removeClass("bottom-trash-drag");
     },
     over: function(event) {
-      console.log("over", event.target);
+      $(event.target).addClass("dropover-active")
     },
     out: function(event) {
-      console.log("out", event.target);
+      $(event.target).removeClass("dropover-active")
     },
     update: function(event) {
       //array to store the task data in
@@ -248,10 +250,11 @@ $(".list-group").on("click", "span", function(){
     tolerance: "touch" , 
     drop: function(event, ui) {
       ui.draggable.remove();
-      console.log("drop");
+      $(".bottom-trash").removeClass("bottom-trash-active");
     },
     over: function(event, ui) {
-      console.log("over");
+      $(".bottom-trash").addClass("bottom-trash-active");
+      $(".bottom-trash").removeClass("bottom-trash-active");
     },
     out: function(event, ui) {
       console.log("out");
@@ -286,6 +289,12 @@ $(".list-group").on("click", "span", function(){
     }
 
   };
+
+  setInterval(function () {
+    $(".card .list-group-item").each(function(index, el) {
+      auditTask(el);
+    });
+  }, (1000 * 60) * 30);
 
 // load tasks for the first time
 loadTasks();
